c4343abc84a32b4c7b4ea1cdb85892708cc2219b
Apex-Coder <micnic123@gmail.com>
2021-08-26 08:53:41 -0400

    Intial Setup Typo fixes

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1 className="project-name">KINGS OF JUDAH</h1>
-      <p>Welcome to the Kings Project. The Kings Project is glossary of sorts for the rulers of Judah and follows
-          the timeline of the throne of Judah from King David onwards. As such Queen Athaliah is listed amoung the kings 
-          because she would have ascended to the throne even though she is not a decendent of David.
+      <p>Welcome to the Kings Project. 
+        The Kings Project is glossary of sorts for the rulers of Judah and follows the timeline of the throne of Judah from King David onwards. 
+        As such Queen Athaliah is listed among the kings because she would have ascended to the throne even though she is not a descendent of David.
       </p> 
       <div className="container">
         <Timeline />
